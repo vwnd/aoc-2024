6d4d852ee76896b7b7a7458dc11ffb5554f6d02d
Victor Wanderley Barbosa <61347148+vwnd@users.noreply.github.com>
2024-12-03 08:38:14 +0100

    part 1 and part 2

--- src/challenges/day_3.rs
+++ src/challenges/day_3.rs
@@ -17,16 +17,36 @@ pub fn solve() {
 
     // let input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
     let input = read_file("src/data/day-3.txt");
-    let expression = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+    let expression = Regex::new(r"(?:mul\((\d+),(\d+)\))|(do\(\)|don't\(\))").unwrap();
     let matches: Vec<_> = expression.find_iter(&input).map(|m| m.as_str()).collect();
 
-    let mut answer = 0;
+    let mut answer_1 = 0;
+    let mut answer_2 = 0;
+    let mut enabled = true;
+
     for m in matches {
-        if let Some((num_1, num_2)) = parse_mul_operation(m) {
-            let result = num_1 * num_2;
-            answer += result;
+        // Part 1
+        if m == "do()" || m == "don't()" {
+        } else {
+            if let Some((num_1, num_2)) = parse_mul_operation(m) {
+                let result = num_1 * num_2;
+                answer_1 += result;
+            }
+        }
+
+        // Part 2
+        if m == "do()" {
+            enabled = true;
+        } else if m == "don't()" {
+            enabled = false;
+        } else if enabled {
+            if let Some((num_1, num_2)) = parse_mul_operation(m) {
+                let result = num_1 * num_2;
+                answer_2 += result;
+            }
         }
     }
 
-    println!("Solution Part 1: {}", answer);
+    println!("Solution Part 1: {}", answer_1);
+    println!("Solution Part 2: {}", answer_2);
 }
